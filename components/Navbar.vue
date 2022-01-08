bc752a0fe341f33a80c77e5af8339fa67ae21435
PGabriel-MB <pablogrbarbosa@gmail.com>
2022-01-08 13:16:23 -0300

    editando e craindo navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-black p-4 fixed top-0 left-0 right-0 z-10">
+    <img width="30" class="rounded-full" src="/img/logo_nome_letras.jpg" alt="">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="relative">
+    <Navbar />
+    <Tutorial/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
