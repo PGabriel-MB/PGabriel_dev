95f5546c0da95712e942124976dff63eb905e02c
PGabriel-MB <pablogrbarbosa@gmail.com>
2022-01-21 21:19:37 -0300

    teste

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,39 @@
 <template>
-  <div class="bg-black p-4 fixed top-0 left-0 right-0 z-10">
-    <img width="30" class="rounded-full" src="/img/logo_nome_letras.jpg" alt="">
-  </div>
+  <nav class="bg-black p-4 fixed top-0 left-0 right-0 z-10">
+    <a class="logoHome">
+      <img width="30" class="rounded-full" src="/img/logo_nome_letras.jpg" alt="Minilogo contendo as iniciais da página">
+    </a>
+
+    <button class="toggleMenuButton">
+      <font-awesome-icon :icon="['fas', 'bars']" />
+    </button>
+  </nav>
+
 </template>
 
-<style scoped>
+<script>
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
+export default {
+  computed: {
+    fas() {
+      return fas
+    }
+  }
+}
+</script>
+
+<style scoped>
+.logoHome:visited {
+  text-decoration: none;
+  decoration: none;
+}
+
+.toggleMenuButton {
+  color: white;
+  padding: 3px 10px;
+  font-size: 1.5em;
+  border: 1px solid grey;
+  border-radius: 10px;
+}
 </style>
